a354baa9915a22d6043b2657e1962a3901fcabd3
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-05-03 00:14:47 +0100

    Release 1, begining version 4

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -5,22 +5,30 @@ import models.Station;
 import play.Logger;
 import play.mvc.Controller;
 
-public class StationCtrl extends Controller
-{
+public class StationCtrl extends Controller {
     public static void index(String name) {
         Station station = Station.find("byName", name).first();
         Logger.info("Station name = " + name);
         render("station.html", station);
     }
-    public static void deletesong (Long id, Long stationid)
-    {
-        Station station = models.Station.findById(id);
-        Reading reading = Reading.findById(stationid);
-        Logger.info ("Removing" + reading.code);
+
+    public static void deleteReading(Long stationId, Long readingId) {
+        Station station = Station.findById(stationId);
+        Reading reading = Reading.findById(readingId);
+        Logger.info("Removing reading with code " + reading.code);
         station.readings.remove(reading);
         station.save();
         reading.delete();
         render("station.html", station);
+    }
+
+    public static void addReading(Long id, int code, float temperature, float windSpeed, int pressure, float windDirection)
+    {
+        Reading reading = new Reading(code, temperature, windSpeed, pressure, windDirection);
+        Station station = Station.findById(id);
+        station.readings.add(reading);
+        station.save();
+        redirect("/stations/" + station.name);
     }
 }
 
